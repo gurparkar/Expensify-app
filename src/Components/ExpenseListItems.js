3829356a3ad15cdb6f267dc87fe29a5055eeb4bb
GURPARKAR SINGH <gurparkarran12@gmail.com>
2020-03-01 01:55:24 +0530

    Formatting add for the Date and Amount

--- src/Components/ExpenseListItems.js
+++ src/Components/ExpenseListItems.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import moment from 'moment'
 //import { connect } from 'react-redux'
 
 import { Link } from 'react-router-dom'
+import numeral from 'numeral'
 export const ExpenseListItems = ({ id,description, amount, createdAt}) => {
     
     return(
@@ -11,7 +13,9 @@ export const ExpenseListItems = ({ id,description, amount, createdAt}) => {
         
     </Link>
     
-    <p>{amount} - {createdAt}</p>
+    <p>{ numeral(amount).format('$0,0.00') }
+     - 
+     { moment(createdAt).format('MMMM Do YYYY') }</p>
 
 
     </div>
