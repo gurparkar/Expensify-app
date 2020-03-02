0154a735a368a118637ce60f6bba1930fb585e6b
GURPARKAR SINGH <gurparkarran12@gmail.com>
2020-03-02 17:39:23 +0530

    Adding heading for totalling Expenses

--- src/Components/ExpenseDashboardPage.js
+++ src/Components/ExpenseDashboardPage.js
@@ -2,9 +2,10 @@ import React from 'react'
 import  ExpenseList from './ExpenseList'
 
 import ExpenseListFilters from './ExpenseListFilters'
-
+import ExpenseSummary from './ExpensesSummary'
 const ExpenseDashboardPage = () => (
     <div>
+        <ExpenseSummary />
         <ExpenseListFilters />
         This is from my dashboard component
         <ExpenseList />
